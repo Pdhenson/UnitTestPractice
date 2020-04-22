8176044b9bea975cfbf7cf954ebe214bd377f1f4
darius <T1ckleT@ckle>
2020-04-22 11:04:39 -0700

    Fixing bug sortDescending unit test

--- PracticeTest.cpp
+++ PracticeTest.cpp
@@ -84,7 +84,7 @@ TEST(PracticeTest, is_not_palindrome_with_space)
 TEST(PracticeTest, is_sorted)
 {
     Practice obj;
-    bool actual = obj.sortDescending(1,3,5);
+    vector <int> actual = push_back(obj.sortDescending(1,3,5));
     ASSERT_GE(actual[0], actual[1]);
     ASSERT_GE(actual[1], actual[2]);
     ASSERT_GE(actual[0], actual[2]);
